e602735e19c3c0026639f0b81c2c876f0406c255
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-13 01:22:54 +0300

    All questions

--- main.js
+++ main.js
@@ -79,7 +79,45 @@ app.use("/users/create",authRouter);
 //--------------------------------------------------------------------
 
 //q3
+authRouter.use("/products",(req,res,next)=>{
+    res.json(users);
+    next();
+})
+app.use("*",authRouter);
+//--------------------------------------------------------------------
 
+//q4 
+const products = ["keyboard","mouse"];
+
+app.get("/products/update",(req,res,next)=>{
+    res.json();
+    next();
+    });
+//--------------------------------------------------------------------
+
+//q5
+authRouter.use("*", (req, res, next) => {
+    console.log(products);
+    next();
+  });
+//--------------------------------------------------------------------
+
+//q6
+/*app.get("*",(req,res,next)=>{
+    const err =new Error("NOT FOUND")
+    err.status = 404
+    next(err)
+})
+​
+app.use((err, req, res, next) => {
+    res.status(err.status);
+    res.json({
+      error: {
+        status: err.status,
+        message: err.message,
+      },
+    });
+  });*/
 //--------------------------------------------------------------------
 app.listen(port,()=>{
 console.log(`Example app listening at http://localhost:${port}`);
